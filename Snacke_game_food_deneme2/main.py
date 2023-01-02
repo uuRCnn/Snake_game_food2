f00057f08d806b30a2d5e1023580419a466d3f7d
Ugurcan <ugurcan380@gmail.com>
2023-01-02 13:49:11 +0300

    Initial commit

---
+++ Snacke_game_food_deneme2/main.py
@@ -0,0 +1,39 @@
+from turtle import Screen
+from Snake import Snake
+from Food import Food
+import turtle as t
+import random
+import time
+
+# flork = t.Turtle()
+#
+# flork.setheading()
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
+
+screen.exitonclick()
